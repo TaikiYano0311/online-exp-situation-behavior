105a40f841e2a39485770a9b88e5c8629e7c0273
Yano <taiki.y.soccer.g@gmail.com>
2025-09-27 15:27:28 +0900

    update

--- pages/tutorial.py
+++ pages/tutorial.py
@@ -2,8 +2,6 @@ import random
 import time
 from functools import reduce
 
-
-
 def add(a, b):
     return a + b
 
@@ -12,14 +10,16 @@ from streamlit_scroll_to_top import scroll_to_here
 
 # random.seed(1234)
 
+#url_turorial = "https://wu-cloud-bucket.s3.ap-northeast-3.amazonaws.com/20250926-carlos-yano-situation-behavior/1-1dd.mp4"
+
 folder = "https://wu-cloud-bucket.s3.ap-northeast-3.amazonaws.com/20250926-carlos-yano-situation-behavior/"
 
-indices = ["1-1", "1-2", "1-3", "2-1", "2-2", "2-3", "2-4", "3-1", "3-2", "3-3"]
+indices = ["1-1"]
 
 
 video_ids = [
     f"{idx}{t}"
-    for t in ["cc", "cd", "dc", "dd"]
+    for t in ["dd"]
     for idx in indices
 ]
 VID2URL = {
@@ -27,54 +27,21 @@ VID2URL = {
     for video_id in video_ids
 }
 
-# Add mapping for idx to Japanese scenario description
-SCENARIO_MAP = {
-    "1-1": "入場受付・大人",
-    "1-2": "入場受付・子供  小学4年生",
-    "1-3": "入場受付・高齢者(軽度の難聴)",
-    "2-1": "迷子対応・大人",
-    "2-2": "迷子対応・子供 5歳",
-    "2-3": "迷子対応・子供 小学4年生",
-    "2-4": "迷子対応・高齢者(軽度の難聴)",
-    "3-1": "(いらだつ客への)苦情対応・大人",
-    "3-2": "(ぐずる客への)苦情対応・子供 5歳",
-    "3-3": "(いらだつ客への)苦情対応・高齢者(軽度の難聴)",
-}
 
 # st.write(video_ids)
 # st.write(VID2URL)
 # assert 0
-N_SCENARIOS = 6
-N_VIDEOS = 4
+N_SCENARIOS = 1
+N_VIDEOS = 1
 
 
 
 if "scenarios" not in st.session_state:
     # Initialize
-    scenarios_A = [
-        {"idx": "1-1", "videos": [["1-1dd", "1-1dc"], ["1-1cc", "1-1cd"]]},
-        {"idx": "1-3", "videos": [["1-3dd", "1-3dc"], ["1-3cc", "1-3cd"]]},
-        {"idx": "2-1", "videos": [["2-1dd", "2-1dc"], ["2-1cc", "2-1cd"]]},
-        {"idx": "2-3", "videos": [["2-3dd", "2-3dc"], ["2-3cc", "2-3cd"]]},
-        {"idx": "3-2", "videos": [["3-2dd", "3-2dc"], ["3-2cc", "3-2cd"]]},
-        {"idx": "3-3", "videos": [["3-3dd", "3-3dc"], ["3-3cc", "3-3cd"]]},
+    scenarios_tutorial = [
+        {"idx": "1-1", "videos": ["1-1dd"],},
     ]
-    scenarios_B = [
-        {"idx": "1-1", "videos": [["1-1dd", "1-1dc"], ["1-1cc", "1-1cd"]]},
-        {"idx": "1-2", "videos": [["1-2dd", "1-2dc"], ["1-2cc", "1-2cd"]]},
-        {"idx": "2-2", "videos": [["2-2dd", "2-2dc"], ["2-2cc", "2-2cd"]]},
-        {"idx": "2-4", "videos": [["2-4dd", "2-4dc"], ["2-4cc", "2-4cd"]]},
-        {"idx": "3-1", "videos": [["3-1dd", "3-1dc"], ["3-1cc", "3-1cd"]]},
-        {"idx": "3-3", "videos": [["3-3dd", "3-3dc"], ["3-3cc", "3-3cd"]]},
-    ]
-    scenarios = scenarios_A
-    # scenarios = scenarios_B
-    for i in range(len(scenarios)):
-        for j in range(len(scenarios[i]["videos"])):
-            random.shuffle(scenarios[i]["videos"][j])
-        random.shuffle(scenarios[i]["videos"])
-        scenarios[i]["videos"] = reduce(add, scenarios[i]["videos"])
-    random.shuffle(scenarios)
+    scenarios = scenarios_tutorial
     # st.write(scenarios)
     st.session_state["scenarios"] = scenarios
     st.session_state["scenario_idx"] = 0
@@ -115,8 +82,8 @@ def on_form_submitted():
             st.session_state[f'q4_choice_{st.session_state["scenario_idx"]}_{idx}']
         )
         data["videos"][vids[idx]] = [q1_value, q2_value, q3_value, q4_value]
-    data["ranking"] = [f"{name}_{vids[idx]}" for idx, name in enumerate(st.session_state[f'ranking_{st.session_state["scenario_idx"]}'])]
-    data["comment"] = st.session_state[f'comment_{st.session_state["scenario_idx"]}']
+    #data["ranking"] = [f"{name}_{vids[idx]}" for idx, name in enumerate(st.session_state[f'ranking_{st.session_state["scenario_idx"]}'])]
+    #data["comment"] = st.session_state[f'comment_{st.session_state["scenario_idx"]}']
     st.session_state["log"].append(data)
 
     # Move to next
@@ -127,7 +94,7 @@ def on_form_submitted():
 
 
 # Interface
-st.title("実験")
+st.title("練習")
 st.warning(
     "注意：ページを更新したり、タブを閉じたり、戻るボタンを押したりしないでください。入力済みのデータが失われます。"
 )
@@ -149,25 +116,12 @@ def exp_fragment():
     # Check if all completed
     if st.session_state["scenario_idx"] == N_SCENARIOS:
         # Move to next page
-        st.switch_page("pages/comment.py")
+        st.switch_page("pages/exp.py")
 
     # Get sample info
-    #vids = st.session_state["scenarios"][st.session_state["scenario_idx"]]["videos"]
-    #urls = [VID2URL[vid] for vid in vids]
-
-    # Added ③
-    # Get sample info
-    scenario_index = st.session_state["scenario_idx"]
-    current_scenario = st.session_state["scenarios"][scenario_index]
-    vids = current_scenario["videos"]
+    vids = st.session_state["scenarios"][st.session_state["scenario_idx"]]["videos"]
     urls = [VID2URL[vid] for vid in vids]
-    scenario_idx_key = current_scenario["idx"]
-    scenario_description = SCENARIO_MAP.get(scenario_idx_key, "不明なシナリオ")
 
-    # Display experiment number and scenario description
-    with st.container(border=True):
-        st.subheader(f"実験{scenario_index + 1}〈{scenario_description}〉")
-        st.markdown(f"実験{scenario_index + 1}を開始します。")
 
     # Place interface
     with st.container(border=True):
@@ -203,40 +157,31 @@ def exp_fragment():
                     key=f'q4_choice_{st.session_state["scenario_idx"]}_{idx}',
                     horizontal=True,
                 )
-        with st.container(border=True):
-            st.subheader("４つのビデオの比較")
-            st.pills(
-                "以上の４つのビデオについて、対応が良いと感じられた順に選択してください。",
-                options=[f"ビデオ{idx+1}" for idx in range(4)],
-                key=f'ranking_{st.session_state["scenario_idx"]}',
-                selection_mode="multi"
-            )
-            ranking = st.session_state[f'ranking_{st.session_state["scenario_idx"]}']
-            st.write(f"あなたが選んだ順位: {ranking}")
-            def reset_ranking():
-                st.session_state[f'ranking_{st.session_state["scenario_idx"]}'] = []
-            st.button("順位をリセット", on_click=reset_ranking)
 
-        with st.container(border=True):
-            st.markdown(
-            """
-            4つのビデオについて、良かった点、悪かった点などのご意見・ご感想等ありましたらご自由にお書きください。
-            """
-            )
-            st.text_area(
-                label="コメント", #placeholder="コメントがあれば入力してください"
-                key=f'comment_{st.session_state["scenario_idx"]}',
-            )
+        #with st.container(border=True):
+        #    st.markdown(
+        #    """4つのビデオについて、良かった点、悪かった点などのご意見・ご感想等ありましたらご自由にお書きください。
+        #    """
+        #    )
+        #    st.text_area(
+        #        label="コメント", #placeholder="コメントがあれば入力してください"
+        #        key=f'comment_{st.session_state["scenario_idx"]}',
+        #    ) """
 
         choice_has_not_been_made = (
             q1_choice is None
             or q2_choice is None
             or q3_choice is None
             or q4_choice is None
-            or len(st.session_state[f'ranking_{st.session_state["scenario_idx"]}']) < 4
+            #or len(st.session_state[f'ranking_{st.session_state["scenario_idx"]}']) < 4
+        )
+        st.markdown(
+            """
+            では、これより本実験を始めます。次のページにお進みください。
+            """
         )
         st.button(
-            "次へ",
+            "実験へ",
             on_click=on_form_submitted,
             disabled=choice_has_not_been_made,
             help="質問にお答えください。" if choice_has_not_been_made else "",
@@ -249,3 +194,4 @@ def exp_fragment():
 
 
 exp_fragment()
+
